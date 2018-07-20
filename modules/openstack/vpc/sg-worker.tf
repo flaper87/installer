02b961c28992c66563092282ae3873448b40bdd5
Flavio Percoco <flaper87@gmail.com>
2018-07-20 14:34:33 +0200

    Egress rules are created by default

--- modules/openstack/vpc/sg-worker.tf
+++ modules/openstack/vpc/sg-worker.tf
@@ -2,17 +2,6 @@ resource "openstack_networking_secgroup_v2" "worker" {
   name = "worker"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "worker_egress" {
-  direction              = "egress"
-  security_group_id = "${openstack_networking_secgroup_v2.worker.id}"
-
-  port_range_min   = 0
-  port_range_max     = 0
-  protocol    = "-1"
-  remote_ip_prefix = "0.0.0.0/0"
-    ethertype = "IPv4"
-}
-
 resource "openstack_networking_secgroup_rule_v2" "worker_ingress_icmp" {
   direction              = "ingress"
   security_group_id = "${openstack_networking_secgroup_v2.worker.id}"
