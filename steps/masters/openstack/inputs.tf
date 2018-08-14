c0a35eb1ac49bc50c057d0ba92c15d5c41ea1e11
Jeremiah Stuever <jstuever@redhat.com>
2018-08-14 16:21:20 -0400

    steps/masters/openstack: organize and fmt variables
    Organize the variables and passed through terraform fmt so that it
    will pass ci tests.

--- steps/masters/openstack/inputs.tf
+++ steps/masters/openstack/inputs.tf
@@ -7,8 +7,8 @@ data "terraform_remote_state" "topology" {
 }
 
 locals {
-  subnet_ids = "${data.terraform_remote_state.topology.subnet_ids_masters}"
-  openstack_lbs    = "${data.terraform_remote_state.topology.openstack_lbs}"
-  sg_id      = "${data.terraform_remote_state.topology.master_sg_id}"
   ignition_bootstrap = "${data.terraform_remote_state.topology.ignition_bootstrap}"
+  openstack_lbs      = "${data.terraform_remote_state.topology.openstack_lbs}"
+  sg_id              = "${data.terraform_remote_state.topology.master_sg_id}"
+  subnet_ids         = "${data.terraform_remote_state.topology.subnet_ids_masters}"
 }
