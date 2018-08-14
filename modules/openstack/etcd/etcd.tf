7d9b3417d460762c3aecaff123ce014c8f36ce57
Jeremiah Stuever <jstuever@redhat.com>
2018-08-14 14:20:06 -0400

    modules/openstack/etcd: organize and fmt variables
    Organize the variables and passed through terraform fmt so that it
    will pass ci tests.

--- modules/openstack/etcd/etcd.tf
+++ modules/openstack/etcd/etcd.tf
@@ -7,7 +7,7 @@ data "null_data_source" "networks" {
 }
 
 data "openstack_images_image_v2" "etcd_img" {
-  name = "${var.base_image}"
+  name        = "${var.base_image}"
   most_recent = true
 }
 
@@ -16,19 +16,19 @@ data "openstack_compute_flavor_v2" "etcd_flavor" {
 }
 
 resource "openstack_compute_instance_v2" "etcd_conf" {
-  count           = "${var.instance_count}"
-  name            = "${var.cluster_name}-etcd-${count.index}"
-  image_id        = "${data.openstack_images_image_v2.etcd_img.id}"
+  name  = "${var.cluster_name}-etcd-${count.index}"
+  count = "${var.instance_count}"
+
   flavor_id       = "${data.openstack_compute_flavor_v2.etcd_flavor.id}"
+  image_id        = "${data.openstack_images_image_v2.etcd_img.id}"
   key_pair        = "${var.ssh_key}"
-  user_data       = "${var.user_data_ign}"
+  network         = ["${data.null_data_source.networks.outputs}"]
   security_groups = ["${var.etcd_sg_ids}"]
+  user_data       = "${var.user_data_ign}"
 
   metadata {
-      Name               = "${var.cluster_name}-etcd"
-      owned              = "kubernetes.io/cluster/${var.cluster_name}"
-      tectonicClusterID  = "${var.cluster_id}"
+    Name              = "${var.cluster_name}-etcd"
+    owned             = "kubernetes.io/cluster/${var.cluster_name}"
+    tectonicClusterID = "${var.cluster_id}"
   }
-
-  network = ["${data.null_data_source.networks.outputs}"]
 }
--- modules/openstack/etcd/outputs.tf
+++ modules/openstack/etcd/outputs.tf
@@ -1,15 +1,15 @@
-output "openstack_compute_instance_v2" {
-  value = "${openstack_compute_instance_v2.etcd_conf.*.id}"
+output "cluster_id" {
+  value = "${var.cluster_id}"
 }
 
-output "subnet_ids" {
-  value = "${var.subnet_ids}"
+output "openstack_compute_instance_v2" {
+  value = "${openstack_compute_instance_v2.etcd_conf.*.id}"
 }
 
 output "openstack_lbs" {
   value = "${var.openstack_lbs}"
 }
 
-output "cluster_id" {
-  value = "${var.cluster_id}"
+output "subnet_ids" {
+  value = "${var.subnet_ids}"
 }
