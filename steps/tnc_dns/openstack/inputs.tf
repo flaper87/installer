a773a0dc745d669747bafaaffa555eaf794b2b55
Jeremiah Stuever <jstuever@redhat.com>
2018-08-14 16:25:12 -0400

    steps/tnc_dns/openstack: organize and fmt variables
    Organize the variables and passed through terraform fmt so that it
    will pass ci tests.

--- steps/tnc_dns/openstack/inputs.tf
+++ steps/tnc_dns/openstack/inputs.tf
@@ -7,8 +7,9 @@
 }
 
 locals {
-  private_zone_id           = "${var.tectonic_openstack_external_private_zone != "" ? var.tectonic_openstack_external_private_zone : data.terraform_remote_state.topology.private_zone_id}"
-  tnc_s3_bucket_domain_name = "${data.terraform_remote_state.topology.tnc_s3_bucket_domain_name}"
   tnc_elb_dns_name          = "${data.terraform_remote_state.topology.tnc_elb_dns_name}"
   tnc_elb_zone_id           = "${data.terraform_remote_state.topology.tnc_elb_zone_id}"
+  tnc_s3_bucket_domain_name = "${data.terraform_remote_state.topology.tnc_s3_bucket_domain_name}"
+  private_zone_id           = "${var.tectonic_openstack_external_private_zone != "" ? var.tectonic_openstack_external_private_zone : data.terraform_remote_state.topology.private_zone_id}"
 }*/
+
